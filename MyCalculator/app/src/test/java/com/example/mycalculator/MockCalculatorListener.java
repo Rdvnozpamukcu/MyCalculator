a7fd70850c301936fbf47ed9b60c4f278e0b7391
Rdvnozpamukcu <rozpamukcu1@gmail.com>
2019-11-25 20:21:21 +0300

    Add files via upload
    Gui lab 10

---
+++ MyCalculator/app/src/test/java/com/example/mycalculator/MockCalculatorListener.java
@@ -0,0 +1,10 @@
+package com.example.mycalculator;
+
+class MockCalculatorListener implements CalculatorListener {
+    int result;
+
+    @Override
+    public void onResultCalculated(int result) {
+        this.result = result;
+    }
+}
